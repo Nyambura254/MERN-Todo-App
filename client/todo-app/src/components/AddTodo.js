7f6fa8c1de7161099938db69c765d901f65177e6
BonnieNyambura <nyamburabonnie@gmail.com>
2020-08-12 15:26:26 +0300

    add todos

--- client/todo-app/src/components/AddTodo.js
+++ client/todo-app/src/components/AddTodo.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Consumer } from '../context'
 
 export default class AddTodo extends Component {
     state = {
@@ -11,14 +12,24 @@ export default class AddTodo extends Component {
             title: e.target.value
         })
     }
+    add = (dispatch, e) => {
+        e.preventDefault()
+        const newTodo = this.state;
+        dispatch({ type: "ADD", payload: newTodo })
+        this.setState({ title: "" })
+    }
     render() {
         return (
+            <Consumer>{value => {
+                //destructuring
+                const { dispatch } = value
+                return <form onSubmit={this.add.bind(this, dispatch)}>
+                    <input type="text" className="form-control rounded-0" placeholder="Write your todos here..." onChange={this.update} value={this.state.title}>
+                    </input>
+                    <button className="form-control rounded-0 btn-secondary" type="submit">Add Todo</button>
+                </form>
+            }}</Consumer>
 
-            <form>
-                <input type="text" className="form-control rounded-0" placeholder="Write your todos here..." onChange={this.update} value={this.state.value}>
-                </input>
-                <button className="form-control rounded-0 btn-secondary" type="submit">Add Todo</button>
-            </form>
         )
     }
 }
