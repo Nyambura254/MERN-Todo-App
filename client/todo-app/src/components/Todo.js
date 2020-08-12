4ba2c8367ebfb3f1f4a3bf67a7e739d74c028ea3
BonnieNyambura <nyamburabonnie@gmail.com>
2020-08-12 12:38:59 +0300

    destructuring todo.js

--- client/todo-app/src/components/Todo.js
+++ client/todo-app/src/components/Todo.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export default class Todo extends Component {
     render() {
         //destructuring
+        const { title } = this.todo
         return (
             <h1 className="text-dark text-center p-1 bg-light border-bottom">
-                <i className="far fa-times-circle fa-sm float-left m-1 text-danger"></i>Task
+                <i className="far fa-times-circle fa-sm float-left m-1 text-danger"></i>{title}
                 <input type="checkbox" className="m-2 float-right"></input>
             </h1>
         )
