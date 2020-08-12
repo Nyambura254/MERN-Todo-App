1236d406bd6595d22e58d55b2f6425bad0540d5b
BonnieNyambura <nyamburabonnie@gmail.com>
2020-08-12 13:05:50 +0300

    hannling checkbox for complete

--- client/todo-app/src/context.js
+++ client/todo-app/src/context.js
@@ -1,37 +1,48 @@
 import React, { Component } from 'react'
 //to create our context
 const Context = React.createContext()
-export class Provider extends Component {
-    state = {
-        todos: [
 
-            {
-                id: 1,
-                title: "React In action",
-                complete: false
-            },
-            {
-                id: 2,
-                title: " MERN App",
-                complete: false
-            },
-            {
-                id: 3,
-                title: "Axios HTTP",
-                complete: false
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case "TOGGLE":
+            return {
+                todos: prevState.todos.map(todos => {
+                    if (todos.id === action.payload) { todos.complete = !todos.complete };
+                    return todos
+                }
+                )
             }
+            export class Provider extends Component {
+                state = {
+                    todos: [
 
-        ],
-        //to handle complete checkbox
-        dispatch: (action) => this.setState(prevState => reducer(prevState, action))
-    }
-    render() {
-        return (
-            <Context.Provider value={this.state}>
-                {this.props.children}
+                        {
+                            id: 1,
+                            title: "React In action",
+                            complete: false
+                        },
+                        {
+                            id: 2,
+                            title: " MERN App",
+                            complete: false
+                        },
+                        {
+                            id: 3,
+                            title: "Axios HTTP",
+                            complete: false
+                        }
 
-            </Context.Provider>
-        )
-    }
-}
-export const Consumer = Context.Consumer;
+                    ],
+                    //to handle complete checkbox with action object
+                    dispatch: (action) => this.setState(prevState => reducer(prevState, action))
+                }
+                render() {
+                    return (
+                        <Context.Provider value={this.state}>
+                            {this.props.children}
+
+                        </Context.Provider>
+                    )
+                }
+            }
+            export const Consumer = Context.Consumer;
