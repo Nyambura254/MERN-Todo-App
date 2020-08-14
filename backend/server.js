7a1699590aae3033c10881e782ab5b6bd3f8cf5f
BonnieNyambura <nyamburabonnie@gmail.com>
2020-08-14 10:08:28 +0300

    backend configurations

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,22 @@ app.use(express.json());
 
 
 
-const db = "mongodb://localhost/myMERNtodo"
+const db = "mongodb://localhost:27017/myMERNtodo"
 mongoose.connect(db, ({ useNewUrlParser: true }))
+    .then(console.log("connected to mongodb"))
+    .catch(err => console.log(err));
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    complete: {
+        type: Boolean,
+        default: false
+    }
+})
+const Todo = mongoose.model('todo', todoSchema)
+app.get("/todos", (req, res) => {
+    Todo.find().then(todo => res.json(todo))
+})
 
 app.listen(5000, () => {
     console.log("the server is running at port 5000")
