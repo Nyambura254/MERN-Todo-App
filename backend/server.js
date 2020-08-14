acd9f0e11b028253ed9cedc613d390b6d4ed9c51
BonnieNyambura <nyamburabonnie@gmail.com>
2020-08-14 11:05:48 +0300

    creating test

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,16 @@ app.use(express.json());
 
 
 
-const db = "mongodb://localhost:27017/myMERNtodo"
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://bonnie:<password>@cluster0.ulgml.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 mongoose.connect(db, ({ useUnifiedTopology: true }))
     .then(console.log("connected to mongodb"))
     .catch(err => console.log(err));
@@ -28,7 +37,7 @@ app.post('todos', (req, res) => {
     const newTodo = new Todo({
         title: req.body.title
     })
-    newTodo.save().then(res => res.json(res))
+    newTodo.save().then(todo => res.json(todo))
 })
 
 app.listen(5000, () => {
